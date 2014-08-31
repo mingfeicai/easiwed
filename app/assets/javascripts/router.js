c85207b8b669c5aff36a54a370683a63bfdf8d24
Mingfei Cai <mingfei@symphonycommerce.com>
2014-08-31 14:45:57 -0700

    fix the route

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -4,10 +4,8 @@
 Easiwed.Router.map(function() {
 	this.resource('about');
 	this.resource('home');
-	this.resource('company', {path: ':company'}, function() {
-		this.resource('problem-list', function () {
-			this.resource('problem', {path: ':id'});
-		});
+	this.resource('problem-list', function () {
+		this.resource('problem', {path: ':id'});
 	});
 	this.resource('posts', function() {
 		this.resource('post', { path: ':id' });
