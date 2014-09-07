ba115a7c8f5b709c8d8fde6ec4f68ea35dc1c062
Mingfei Cai <mingfei@symphonycommerce.com>
2014-09-07 14:55:20 -0700

    adding page link

--- app/assets/javascripts/controllers/problem_list_controller.js
+++ app/assets/javascripts/controllers/problem_list_controller.js
@@ -1,18 +1,48 @@
 // for more details see: http://emberjs.com/guides/controllers/
 
 Easiwed.ProblemListController = Ember.Controller.extend({
-	queryParams: ['category'],
-	category: null,
-	filteredProblems: function() {
-		var category = this.get('category');
-		var problems = this.get('model').problems;
+	problemsPerPage: 5,
+	queryParams: ['topics'],
+	queryParams: ['page'],
+	topics: null,
+	page: null,
 
-		if (category) {
-			return problems.filter(function(item) {
-				return item.get('category').get('name') == category;
+	filteredProblems: function() {
+		var topics = this.get('topics');
+		var allProblems = this.get('model').problems;
+
+		var filteredProblems = allProblems;
+		if (topics) {
+			filteredProblems = allProblems.filter(function(item) {
+				return item.get('tags').any(function(tag){
+					return tag.get('name').toUpperCase() === topics.toUpperCase();
+				})
 			});
-		} else {
-			return problems;
 		}
-	}.property('category', 'model')
+		return filteredProblems;
+	}.property('topics', 'model'),
+
+	problemsOnCurrentPage: function() {
+		var page = this.get('page');
+		var problemsPerPage = this.get('problemsPerPage');
+		var filteredProblems = this.get('filteredProblems');
+
+		var pageStartIndex = (page - 1) * problemsPerPage;
+		var paginatedProblemIndices = [];
+		for (var i = 0; i < problemsPerPage; i++) {
+			paginatedProblemIndices.push(pageStartIndex + i);
+		}
+		return filteredProblems.objectsAt(paginatedProblemIndices);
+	}.property('filteredProblems', 'page', 'problemsPerPage'),
+
+	pages: function(){
+		var allPages = [];
+		var problemsPerPage = this.get('problemsPerPage');
+		var filteredProblems = this.get('filteredProblems');
+		var allPageNumber =Math.ceil(filteredProblems.get('length') / problemsPerPage);
+		for(var i = 1; i <= allPageNumber; i++ ) {
+			allPages.push(i);
+		}
+		return allPages;
+	}.property('filteredProblems', 'problemsPerPage')
 });
