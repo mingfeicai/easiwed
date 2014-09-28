623af744d4bba5cb379f508aa9e1132d0dabe60f
Mingfei Cai <mingfei@symphonycommerce.com>
2014-09-28 11:30:03 -0700

    front end

--- app/assets/javascripts/controllers/problem_list_controller.js
+++ app/assets/javascripts/controllers/problem_list_controller.js
@@ -2,24 +2,42 @@
 
 Easiwed.ProblemListController = Ember.Controller.extend({
 	problemsPerPage: 5,
-	queryParams: ['topics', 'page'],
-	topics: null,
-	page: null,
+	queryParams: ['topics', 'challenge-min', 'challenge-max', 'page'],
+	isFilterPaneOpen: false,
 
 	filteredProblems: function() {
-		var topics = this.get('topics');
+		var topics = this.get('topics') && this.get('topics').split(',');
+		var challengeMin = this.get('challenge-min');
+		var challengeMax = this.get('challenge-max');
 		var allProblems = this.get('model').problems;
 
 		var filteredProblems = allProblems;
 		if (topics) {
-			filteredProblems = allProblems.filter(function(item) {
-				return item.get('tags').any(function(tag){
-					return tag.get('name').toUpperCase() === topics.toUpperCase();
-				})
+			filteredProblems = filteredProblems.filter(function(item) {
+				var allTags = item.get('tags').map(function(topic){
+					return topic.get('name').toUpperCase();
+				}).uniq();
+				var allTopics = topics.map(function(topic){
+					return topic.toUpperCase();
+				}).uniq();
+				// check if allTags contain topics
+				return allTags.filter(function (tag) {
+					return allTopics.indexOf(tag) > -1;
+				}).length == allTopics.length;
+			});
+		}
+		if(challengeMin) {
+			filteredProblems = filteredProblems.filter(function(item){
+				return item.get('challenge') >= challengeMin;
+			});
+		}
+		if(challengeMax) {
+			filteredProblems = filteredProblems.filter(function(item){
+				return item.get('challenge') <= challengeMax;
 			});
 		}
 		return filteredProblems;
-	}.property('topics', 'model'),
+	}.property('topics', 'model', 'challenge-min', 'challenge-max'),
 
 	problemsOnCurrentPage: function() {
 		var page = this.get('page');
@@ -43,5 +61,36 @@ Easiwed.ProblemListController = Ember.Controller.extend({
 			allPages.push(i);
 		}
 		return allPages;
-	}.property('filteredProblems', 'problemsPerPage')
+	}.property('filteredProblems', 'problemsPerPage'),
+	filters: function(){
+		var allFilters = [];
+		var topics = this.get('topics') && this.get('topics').split(',');
+		var challengeMin = this.get('challenge-min');
+		var challengeMax = this.get('challenge-max');
+		if(topics) {
+			topics.forEach(function(topic){
+				allFilters.push(topic);
+			})
+		}
+		if(challengeMin && challengeMax) {
+			if(challengeMin === challengeMax) {
+				allFilters.push("Challenge:"+ challengeMin);
+			} else {
+				allFilters.push("Challenge:" + challengeMin + "-" + challengeMax);
+			}
+		} else if(challengeMin && !challengeMax) {
+			allFilters.push("Challenge "+ "> " + challengeMin);
+		} else if(!challengeMin && challengeMax) {
+			allFilters.push("Challenge "+ "< " + challengeMax);
+		}
+		return allFilters;
+	}.property('topics', 'challenge-min', 'challenge-max'),
+	actions: {
+		openFilterPane: function () {
+			this.set('isFilterPaneOpen', true);
+		},
+		closeFilterPane: function () {
+			this.set('isFilterPaneOpen', false);
+		}
+	}
 });
